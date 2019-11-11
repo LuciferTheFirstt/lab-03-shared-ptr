5e55d5a2e56fea4a254539fb9e4535c5d8458a5a
LuciferTheFirstt <dimonator67@gmail.com>
2019-11-11 12:03:01 +0300

    all

---
+++ include/SharedPtr.hpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <atomic>
+#include "pch.h"
+
+class Count {
+public:
+	Count() {
+		cnt = 0;
+	}
+
+	void increase() {
+		cnt++;
+	}
+	void decrease() {
+		cnt--;
+	}
+	int refcount() {
+		return cnt;
+	}
+
+private:
+	std::atomic_uint cnt;
+};
+
+
+
+template <typename T>
+class SharedPtr {
+public:
+	SharedPtr() :ptr(nullptr),count(nullptr) {};//По умолчанию
+	SharedPtr(T* pobject)//Принимает на вход указатель
+	{
+		ptr =pobject;
+		count = new Count();
+		count->increase();
+	};
+	SharedPtr(const SharedPtr& r)// Копрование 
+	{
+		ptr = r.ptr;
+		count = r.count;
+		count->increase();
+	};
+	SharedPtr(SharedPtr&& r)//Конструктор перемещения 
+	{
+		ptr = r.ptr;
+		count = r.count;
+		r.ptr = nullptr;
+		r.count = nullptr;
+	}
+
+	~SharedPtr()
+	{
+		if (count != nullptr)
+		{
+			if (count->refcount() > 1)
+			{
+				count->decrease();
+			}
+			else
+			{
+				delete ptr;
+				delete count;
+			}
+		}
+	}
+
+	auto opeartor = (const SharedPtr& r)->SharedPtr&
+	{
+		ptr = r.ptr;
+		count = r.count;
+		count->increase();
+	}
+
+	auto opeartor = (SharedPtr&& r)->SharedPtr&
+	{
+		ptr = r.ptr;
+		count = r.count;
+		r.ptr = nullptr;
+		r.count = nullptr;
+	}
+
+	// проверяет, указывает ли указатель на объект
+	operator bool() const
+	{
+		return ptr != nullptr;
+	}
+	auto operator*() const->T&
+	{
+		return *ptr;
+	}
+
+	auto operator->() const->T*
+	{
+		return ptr;
+	}
+
+	auto get()->T*
+	{
+		return ptr;
+	}
+
+	void reset()
+	{
+		*this = SharedPtr();
+	}
+	void reset(T* ptr)
+	{
+		*this = SharedPtr(ptr);
+	}
+	void swap(SharedPtr& r)
+	{
+		if (&r == this)
+		{
+			return;
+		}
+
+		std::swap(ptr, r.ptr);
+		std::swap(count, r.count);
+	}
+	// возвращает количество объектов SharedPtr, которые ссылаются на тот же управляемый объект
+	auto use_count() const->size_t
+	{
+		if (ptr != nullptr)
+		{
+			return *count;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+private:
+	T* ptr;
+	Count* count;
+};
+
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,168 @@
-// Copyright 2018 Your Name <your_email>
-
 #include <gtest/gtest.h>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
-}
+#include "SharedPtr.hpp"
+
+TEST(shared_ptr, NullCostructor)
+	{
+		SharedPtr<int> ptr;
+
+		EXPECT_EQ(ptr.get(),nullptr);
+		EXPECT_EQ(bool(ptr),false);
+		EXPECT_EQ(ptr.use_count(),0);
+	}
+
+TEST(shared_ptr, ItemInsideCostructor)
+	{
+		SharedPtr ptr(new int{5});
+
+		EXPECT_EQ(*(ptr.get()),5);
+		EXPECT_EQ(bool(ptr),true);
+		EXPECT_EQ(ptr.use_count(),1);
+	}
+
+TEST(shared_ptr, PtrInsideCostructor1)
+			{
+			  SharedPtr ptr(new int{5});
+
+				SharedPtr ptr2(ptr);
+
+				EXPECT_EQ(*(ptr.get()),*(ptr2.get()));
+				EXPECT_EQ(*(ptr.get()),5);
+
+				EXPECT_EQ(bool(ptr),bool(ptr2));
+				EXPECT_EQ(bool(ptr),true);
+
+				EXPECT_EQ(ptr.use_count(),ptr2.use_count());
+				EXPECT_EQ(ptr.use_count(),2);
+			}
+
+TEST(shared_ptr, PtrInsideCostructor2)
+		{
+		  SharedPtr ptr(new int{5});
+
+			SharedPtr ptr2(std::move(ptr));
+
+			EXPECT_EQ(*(ptr2.get()),5);
+			EXPECT_EQ(bool(ptr2),true);
+			EXPECT_EQ(ptr2.use_count(),1);
+		}
+
+TEST(shared_ptr, DeleteTest)
+				{
+				  SharedPtr ptr(new int{5});
+
+					SharedPtr ptr2 = ptr;
+
+					EXPECT_EQ(*(ptr.get()),*(ptr2.get()));
+					EXPECT_EQ(*(ptr.get()),5);
+
+					EXPECT_EQ(bool(ptr),bool(ptr2));
+					EXPECT_EQ(bool(ptr),true);
+
+					EXPECT_EQ(ptr.use_count(),ptr2.use_count());
+					EXPECT_EQ(ptr.use_count(),2);
+
+					ptr.~SharedPtr();
+
+					EXPECT_EQ(*(ptr2.get()),5);
+					EXPECT_EQ(bool(ptr2),true);
+					EXPECT_EQ(ptr2.use_count(),1);
+				}
+
+TEST(shared_ptr, OperatorPrisvaivania1)
+	{
+	  SharedPtr ptr(new int{5});
+
+		SharedPtr ptr2 = ptr;
+
+		EXPECT_EQ(*(ptr.get()),*(ptr2.get()));
+		EXPECT_EQ(*(ptr.get()),5);
+
+		EXPECT_EQ(bool(ptr),bool(ptr2));
+		EXPECT_EQ(bool(ptr),true);
+
+		EXPECT_EQ(ptr.use_count(),ptr2.use_count());
+		EXPECT_EQ(ptr.use_count(),2);
+	}
+
+TEST(shared_ptr, OperatorPrisvaivania2)
+	{
+	  SharedPtr ptr(new int{5});
+
+		SharedPtr ptr2 = std::move(ptr);
+
+		EXPECT_EQ(*(ptr2.get()),5);
+		EXPECT_EQ(bool(ptr2),true);
+		EXPECT_EQ(ptr2.use_count(),1);
+	}
+
+TEST(shared_ptr, IntInsideCostructor)
+		{
+			SharedPtr ptr(new int{5});
+
+			EXPECT_EQ(*(ptr.get()),5);
+			EXPECT_EQ(bool(ptr),true);
+			EXPECT_EQ(ptr.use_count(),1);
+
+			SharedPtr ptr2(ptr);
+
+			EXPECT_EQ(*(ptr2.get()),5);
+			EXPECT_EQ(bool(ptr2),true);
+			EXPECT_EQ(ptr.use_count(),ptr2.use_count());
+			EXPECT_EQ(ptr.use_count(),2);
+
+			ptr.~SharedPtr();
+
+			EXPECT_EQ(*(ptr2.get()),5);
+			EXPECT_EQ(bool(ptr2),true);
+			EXPECT_EQ(ptr2.use_count(),1);
+		}
+
+TEST(shared_ptr, OperatorBool)
+	{
+		SharedPtr<int> ptr{};
+
+		EXPECT_EQ(bool(ptr),false);
+
+		SharedPtr ptr2(new int{5});
+
+		EXPECT_EQ(bool(ptr2),true);
+	}
+
+TEST(shared_ptr, RESET)
+	{
+		SharedPtr ptr{new int{5}};
+
+		ptr.reset();
+		EXPECT_EQ(bool(ptr),false);
+
+		ptr.reset(new int{5});
+		EXPECT_EQ(*(ptr.get()),5);
+		EXPECT_EQ(bool(ptr),true);
+		EXPECT_EQ(ptr.use_count(),1);
+	}
+
+TEST(shared_ptr, SWAP)
+	{
+		SharedPtr ptr{new int{5}};
+		SharedPtr ptr2{new int{105}};
+
+		ptr.swap(ptr2);
+
+		EXPECT_EQ(*(ptr2.get()),5);
+		EXPECT_EQ(bool(ptr2),true);
+		EXPECT_EQ(ptr2.use_count(),1);
+
+		EXPECT_EQ(*(ptr.get()),105);
+		EXPECT_EQ(bool(ptr),true);
+		EXPECT_EQ(ptr.use_count(),1);
+	}
+
+TEST(shared_ptr, Count)
+	{
+		int a = rand();
+		SharedPtr ptr{new int {a}};
+		EXPECT_EQ(ptr.use_count(),1);
+	}
+
+
